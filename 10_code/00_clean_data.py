f6dfd68db84fc389c7a312120b599883ca430721
Bárbara Flores <bpf17@duke.edu>
2024-04-02 18:00:15 -0400

    clean data

--- 10_code/00_clean_data.py
+++ 10_code/00_clean_data.py
@@ -2,7 +2,6 @@ import pandas as pd
 from patsy import dmatrices
 from sklearn.preprocessing import StandardScaler
 
-
 path = "https://raw.githubusercontent.com/nogibjj/IDS705_FinalProject_Lemur_Team/main/00_original_data/adult.csv"
 data = pd.read_csv(path)
 
@@ -13,15 +12,14 @@ data.insert(0, 'income', data.pop('income'))
 data['native-country_United-States'] = (data['native-country'] == 'United-States').astype(int)
 data.drop('native-country', axis=1, inplace=True)
 
-numeric_cols = data.select_dtypes(include=['int64', 'float64']).columns
+numeric_cols = ['age', 'fnlwgt', 'education-num', 'capital-gain',
+       'capital-loss', 'hours-per-week', 'native-country_United-States']
 scaler = StandardScaler()
 data[numeric_cols] = scaler.fit_transform(data[numeric_cols])
 
-
 categorical_cols = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex']
-
 data_ohe = pd.get_dummies(data, columns=categorical_cols)
 
-output_path = "../01_clean_data/adultohe.csv" 
+output_path = "../01_clean_data/adult_ohe.csv" 
 data_ohe.to_csv(output_path, index=False)
 
