82ad20ef5e24bad1552d510aa1e5fd2171372666
Bárbara Flores <bpf17@duke.edu>
2024-04-02 16:39:12 -0400

    update clean data

--- 10_code/00_clean_data.py
+++ 10_code/00_clean_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from patsy import dmatrices
+from sklearn.preprocessing import StandardScaler
+
 
 path = "https://raw.githubusercontent.com/nogibjj/IDS705_FinalProject_Lemur_Team/main/00_original_data/adult.csv"
 data = pd.read_csv(path)
@@ -13,8 +15,6 @@ data.insert(0, 'income', data.pop('income'))
 categorical_cols = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country']
 
 data_ohe = pd.get_dummies(data, columns=categorical_cols)
-
-data_ohe.info()
 
 output_path = "../01_clean_data/adult_ohe.csv" 
 data_ohe.to_csv(output_path, index=False)
