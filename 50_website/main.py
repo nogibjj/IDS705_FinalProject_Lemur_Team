15a77c18490bd71497d0c5750c8d87e30c55ba98
Jeremy Tan <jmt132@duke.edu>
2024-04-14 09:13:44 -0400

    finish website

--- 50_website/main.py
+++ 50_website/main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import pickle
-import sys
 import path
 import os
 
@@ -64,17 +62,43 @@ feature_names = [
     "marital-status",
     "hours-per-week",
     "occupation",
-    "relationship",
+    "education-num",
     "workclass",
+]
+
+feature_names2 = [
+    "age",
+    "workclass",
+    "education-num",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "capital-gain",
+    "capital-loss",
+    "hours-per-week",
+    "native-country_United-States",
 ]
 
 
 def create_input_form(short=True):
     inputs = {}
-    for feature in feature_names:
+    fts = feature_names if short else feature_names2
+    name = "simple" if short else "long"
+    for feature in fts:
         if feature == "age":
             inputs[feature] = st.slider(
-                "Age", min_value=18, max_value=100, step=1, value=30
+                "Age", min_value=18, max_value=100, step=1, value=30, key="1" + name
+            )
+        elif feature == "education-num":
+            inputs[feature] = st.slider(
+                "Education Level",
+                min_value=1,
+                max_value=16,
+                step=1,
+                value=1,
+                key="2" + name,
             )
         elif feature == "marital-status":
             inputs[feature] = st.selectbox(
@@ -88,10 +112,16 @@ def create_input_form(short=True):
                     "Married, Armed Forces Spouse",
                     "Widowed",
                 ],
+                key="3" + name,
             )
         elif feature == "hours-per-week":
             inputs[feature] = st.slider(
-                "Hours per Week", min_value=0, max_value=100, step=1, value=40
+                "Hours per Week",
+                min_value=0,
+                max_value=100,
+                step=1,
+                value=40,
+                key="4" + name,
             )
         elif feature == "occupation":
             inputs[feature] = st.selectbox(
@@ -113,6 +143,7 @@ def create_input_form(short=True):
                     "Private Household Service",
                     "Armed Forces",
                 ],
+                key="5" + name,
             )
         elif feature == "relationship":
             inputs[feature] = st.selectbox(
@@ -125,6 +156,7 @@ def create_input_form(short=True):
                     "Own Child",
                     "Wife",
                 ],
+                key="6" + name,
             )
         elif feature == "workclass":
             inputs[feature] = st.selectbox(
@@ -140,6 +172,35 @@ def create_input_form(short=True):
                     "Without Pay",
                     "Never Worked",
                 ],
+                key="7" + name,
+            )
+        elif feature == "race":
+            inputs[feature] = st.selectbox(
+                "Race",
+                ["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"],
+                key="8" + name,
+            )
+        elif feature == "sex":
+            inputs[feature] = st.selectbox(
+                "Sex",
+                ["Male", "Female"],
+                key="9" + name,
+            )
+        elif feature == "capital-gain":
+            inputs[feature] = st.number_input(
+                "Capital Gain",
+                key="10" + name,
+            )
+        elif feature == "capital-loss":
+            inputs[feature] = st.number_input(
+                "Capital Loss",
+                key="12" + name,
+            )
+        elif feature == "native-country_United-States":
+            inputs[feature] = st.selectbox(
+                "Native Country",
+                ["United States", "Outside of United States"],
+                key="13" + name,
             )
 
     return inputs
@@ -148,25 +209,58 @@ def create_input_form(short=True):
 # make website
 def main():
     # add title
-    st.title("Income Level Predictor")
+    st.title("Income Level Model Presentation")
     # add picture
+    st.image(path + "/final.jpg")
     # add form
-    tab1, tab2 = st.tabs(["Top-6 Features Catboost Model", "Full Catboost Model"])
+    tab1, tab2 = st.tabs(["Top-6 Common Features", "Full Catboost Model"])
     with tab1:
         inputs = create_input_form()
-        submitted = st.button("Submit", type="primary")
+        submitted = st.button(
+            "Submit",
+            type="primary",
+            key=3232,
+        )
         if submitted:
-            print("pressed")
-            print(inputs)
             # convert the variables to the ones that were made readble to the models
             inputs["marital-status"] = ms_read[inputs["marital-status"]]
             inputs["occupation"] = occup_read[inputs["occupation"]]
-            inputs["relationship"] = relationship_read[inputs["relationship"]]
+            # inputs["relationship"] = relationship_read[inputs["relationship"]]
             inputs["workclass"] = workclass_read[inputs["workclass"]]
             pred_df = pd.DataFrame([inputs], columns=feature_names)
             print(pred_df)
             # load model
             path_to_model = path + "/websitemodel2.pkl"
+            with open(path_to_model, "rb") as file:
+                loaded_model = pickle.load(file)
+            # make prediction
+            y_pred_proba = loaded_model.predict_proba(pred_df)[:, 1]
+            # write prediction
+            st.write(
+                f"Probability of being in the upper and middle income class: {round(float(y_pred_proba)*100, 2)}%"
+            )
+    with tab2:
+        inputs2 = create_input_form(
+            short=False,
+        )
+        submitted = st.button(
+            "Submit",
+            type="primary",
+            key=12312,
+        )
+        if submitted:
+            print(inputs2)
+            # convert the variables to the ones that were made readble to the models
+            inputs2["marital-status"] = ms_read[inputs2["marital-status"]]
+            inputs2["occupation"] = occup_read[inputs2["occupation"]]
+            inputs2["relationship"] = relationship_read[inputs2["relationship"]]
+            inputs2["workclass"] = workclass_read[inputs2["workclass"]]
+            inputs2["native-country_United-States"] = (
+                1 if inputs2["native-country_United-States"] == "United States" else 0
+            )
+            pred_df = pd.DataFrame([inputs2], columns=feature_names2)
+            # load model
+            path_to_model = path + "/websitemodel1.pkl"
             with open(path_to_model, "rb") as file:
                 loaded_model = pickle.load(file)
             # make prediction
